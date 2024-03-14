08ab8753458ab03f93006fb6b4fa15b132cd952b
Pabitra Sahoo <37178585+PabitraSahoo@users.noreply.github.com>
2024-03-14 19:38:46 +0530

    13

--- models/task.js
+++ models/task.js
@@ -11,3 +11,33 @@ const taskSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Task', taskSchema);
+
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  taskName: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  catagory: {
+    type: String,
+    trim: true,
+  },
+  createDate: Date,
+  //completedDate: Date,
+  //completed: Boolean,
+  price: {
+    type: Number,
+    required: true // Assuming price is a required field
+  }
+  pID: {
+    type: Number,
+    required: true // Assuming price is a required field
+  }
+});
+
+module.exports = mongoose.model('Task', taskSchema);
